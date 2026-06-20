eaff17347734d313188832892b90f1cd1ba6077a
Paul Lee <paul@strivenword.com>
2026-06-20 04:01:44 -0400

    Main init.

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ program begin: Saturday, Jun 20, 2026 -- at 2:45 a.m. EST
 program: Dark and Drealy Lands
 re-creation in Python of a z-code IFComp entry from 2005
 """
+
+import sys
+
+# def main():
+# 	# forthcoming
+
+if __name__ == "__main__":
+	main()
